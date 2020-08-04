81a9b30507e2bcb2739af3d99f08a24ac3ab53d3
Vinh Phạm <45289894+VinhPham26@users.noreply.github.com>
2020-08-04 14:49:40 +0700

    barely done

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const puppeteer = require("puppeteer");
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto("https://ln.hako.re/sang-tac/7528-mot-the-gioi-dieu-ki");
-    await page.setViewport({ width: 360, height: 740, deviceScaleFactor: 3 });
+    await page.setViewport({ width: 360, height: 740, deviceScaleFactor: 3 }); //viewport cho ss s8
 
-    const chapter = await page.evaluate(() => {
+    const chapters = await page.evaluate(() => {
         let items = document.querySelectorAll(".chapter-name");
         let links = [];
         items.forEach(item => {
@@ -17,6 +17,11 @@ const puppeteer = require("puppeteer");
         });
         return links;
     });
-    console.log(chapter);
+
+    for (let chapter of chapters) {
+        await page.goto(`${chapter.url}`);
+        await page.screenshot({ path: `${chapter.title}.png`, fullPage: true });
+    }
+
     await browser.close();
 })();
