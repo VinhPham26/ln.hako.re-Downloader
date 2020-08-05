1ab9783b2d8ae17021a5f8cc527e8878e6539588
Vinh Phạm <45289894+VinhPham26@users.noreply.github.com>
2020-08-05 19:53:13 +0700

    barely done

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto("https://ln.hako.re/sang-tac/7528-mot-the-gioi-dieu-ki");
-    await page.setViewport({ width: 360, height: 740, deviceScaleFactor: 3 }); //viewport cho ss s8
+    await page.setViewport({ width: 360, height: 740, deviceScaleFactor: 2 }); //viewport for samsung s8
+    await page.goto("https://ln.hako.re/truyen/3569-in-no-jitsury-okusha-ni-naritakute-new");
+    //await page.click('#guest-menu');
 
     const chapters = await page.evaluate(() => {
         let items = document.querySelectorAll(".chapter-name");
@@ -18,9 +20,25 @@ const puppeteer = require("puppeteer");
         return links;
     });
 
-    for (let chapter of chapters) {
-        await page.goto(`${chapter.url}`);
-        await page.screenshot({ path: `${chapter.title}.png`, fullPage: true });
+    try {
+        for (let chapter of chapters) {
+            let chapterName = chapter.title.replace(/[|&;?:$%@/"<>()+,]/g, ""); //remove illegal character for file name
+            let path = `pics/${chapterName}.png`;
+
+            if (fs.existsSync(path)) {
+                continue;
+            }
+            else {
+                await page.goto(`${chapter.url}`, {
+                    waitUntil: 'networkidle0',
+                });
+                await page.waitFor('#chapter-comments', { visible: true }) //wait for comment to show to make sure all the content are fully loaded
+                await page.screenshot({ path: path, fullPage: true });
+                await console.log(`${chapterName}`);
+            }
+        }
+    } catch (err) {
+        console.log(err)
     }
 
     await browser.close();
