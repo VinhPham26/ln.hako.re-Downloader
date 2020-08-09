8c7fa7eb51ca662b1c02c507840a9f5d96669c31
Vinh Phạm <45289894+VinhPham26@users.noreply.github.com>
2020-08-09 17:35:18 +0700

    dark mode
    turn on dark mode of site, add some tweak to reduce image been captured too soon

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const { waitForDebugger } = require('inspector');
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.setViewport({ width: 360, height: 740, deviceScaleFactor: 2 }); //viewport for samsung s8
-    await page.goto("https://ln.hako.re/truyen/3569-in-no-jitsury-okusha-ni-naritakute-new");
-    //await page.click('#guest-menu');
+    await page.setViewport({ width: 360, height: 740, deviceScaleFactor: 2 });  //viewport for samsung s8
+    await page.goto("https://ln.hako.re/truyen/3601-maou-gakuin-no-futekigousha");   //go to the serie main page
 
+    // //turn on night mode of the site
+    await page.click('#guest-menu');
+    await page.click('.nightmode-toggle');
+    await page.waitForSelector('.series-name')    //wait for hako change to night mode
+
+    //get serie name
+    const serie = await page.evaluate(() => {
+        var serieName = document.querySelector('.series-name')
+        return serieName.innerText.replace(/[|&;?:$%@/"<>()+,]/g, "");
+    })
+
+    //Create folder with serie name
+    fs.mkdirSync(serie, { recursive: true })
+
+    //get all the chapters link and title
     const chapters = await page.evaluate(() => {
         let items = document.querySelectorAll(".chapter-name");
         let links = [];
@@ -20,10 +35,11 @@ const fs = require('fs');
         return links;
     });
 
+    //download all the chapters
     try {
         for (let chapter of chapters) {
             let chapterName = chapter.title.replace(/[|&;?:$%@/"<>()+,]/g, ""); //remove illegal character for file name
-            let path = `pics/${chapterName}.png`;
+            let path = `${serie}/${chapterName}.png`;
 
             if (fs.existsSync(path)) {
                 continue;
@@ -32,8 +48,9 @@ const fs = require('fs');
                 await page.goto(`${chapter.url}`, {
                     waitUntil: 'networkidle0',
                 });
-                await page.waitFor('#chapter-comments', { visible: true }) //wait for comment to show to make sure all the content are fully loaded
+                await page.waitForSelector('#chapter-comments') //wait for comment to show to make sure all the content are fully loaded
                 await page.screenshot({ path: path, fullPage: true });
+                //await page.pdf({ path: path })
                 await console.log(`${chapterName}`);
             }
         }
