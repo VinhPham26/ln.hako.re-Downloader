12e57b1dde97b7f9ca48c24f36413f25f55d60ad
Vinh Phạm <45289894+VinhPham26@users.noreply.github.com>
2020-08-10 14:51:22 +0700

    update volume
    create volume folder and download chapter into each volume (still cant fix the bug with white img)

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const { waitForDebugger } = require('inspector');
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.setViewport({ width: 360, height: 740, deviceScaleFactor: 2 });  //viewport for samsung s8
-    await page.goto("https://ln.hako.re/truyen/3601-maou-gakuin-no-futekigousha");   //go to the serie main page
+    await page.setViewport({ width: 360, height: 740, deviceScaleFactor: 2, isMobile: true });  //viewport for samsung s8
+    //set device scale to 1 fix the white in some image
+    await page.goto("https://ln.hako.re/truyen/7278-another-monster");   //go to the serie main page
 
     // //turn on night mode of the site
     await page.click('#guest-menu');
@@ -22,41 +22,54 @@ const { waitForDebugger } = require('inspector');
     //Create folder with serie name
     fs.mkdirSync(serie, { recursive: true })
 
-    //get all the chapters link and title
-    const chapters = await page.evaluate(() => {
-        let items = document.querySelectorAll(".chapter-name");
-        let links = [];
+    //get all the volume
+    const volumes = await page.evaluate(() => {
+        let items = document.querySelectorAll('.volume-list')
+        let lists = [];
         items.forEach(item => {
-            links.push({
-                title: item.innerText.replace(/[\n\r]+|[\s]{2,}/g, ' ').replace('Mới', '').trim(),
-                url: item.querySelector('a').href
-            });
-        });
-        return links;
+            lists.push({
+                title: item.innerText,
+                link: item.querySelector('a').href,
+            })
+        })
+        return lists;
     });
 
-    //download all the chapters
-    try {
+    for await (const volume of volumes) {
+        let volumeName = volume.title.replace(/[|&;?:$%@/"<>()+*,]/g, ""); //remove illegal character for file name
+        //Create folder with volume name
+        fs.mkdirSync(`${serie}/${volumeName}`, { recursive: true })
+        await page.goto(volume.link)
+        await console.log(volumeName)
+
+        //get the chapter title and url
+        const chapters = await page.evaluate(() => {
+            let items = document.querySelectorAll(".chapter-name");
+            let links = [];
+            items.forEach(item => {
+                links.push({
+                    title: item.innerText.replace(/[\n\r]+|[\s]{2,}/g, ' ').replace('Mới', '').trim(),
+                    url: item.querySelector('a').href
+                });
+            });
+            return links;
+        });
+
+        //download all the chapters
         for (let chapter of chapters) {
             let chapterName = chapter.title.replace(/[|&;?:$%@/"<>()+,]/g, ""); //remove illegal character for file name
-            let path = `${serie}/${chapterName}.png`;
+            let path = `${serie}/${volumeName}/${chapterName}.png`;
 
             if (fs.existsSync(path)) {
                 continue;
             }
             else {
-                await page.goto(`${chapter.url}`, {
-                    waitUntil: 'networkidle0',
-                });
-                await page.waitForSelector('#chapter-comments') //wait for comment to show to make sure all the content are fully loaded
+                await page.goto(`${chapter.url}`, { waitUntil: 'networkidle0', });
+                await page.waitForSelector('.sect-title') //wait for comment to show to make sure all the content are fully loaded
                 await page.screenshot({ path: path, fullPage: true });
-                //await page.pdf({ path: path })
-                await console.log(`${chapterName}`);
+                await console.log(` ${chapterName}`);
             }
         }
-    } catch (err) {
-        console.log(err)
     }
-
-    await browser.close();
+    await browser.close()
 })();
